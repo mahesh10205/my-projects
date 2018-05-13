b149a3b7684090d023ced5743d9f520d2107b35c
mahesh10205 <mahesh10205@gmail.com>
2018-05-13 01:41:43 -0500

    Added project

---
+++ front-end/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+   allowServer: boolean=false;
+  constructor() { 
+    setTimeout(()=>{
+      this.allowServer=true;
+    },2000)
+  }
+
+  ngOnInit() {
+  }
+
+}
